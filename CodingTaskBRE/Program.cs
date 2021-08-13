51835ed87ceb7b15e52f50dbeb17049b9d7112cb
CipherSoftRepo <38687589+CipherSoftRepo@users.noreply.github.com>
2021-08-13 10:42:47 +0200

    code

---
+++ CodingTaskBRE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodingTaskBRE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

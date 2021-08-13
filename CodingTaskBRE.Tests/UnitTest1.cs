ab69b99382e41f4dd94aa37baeccb993ef4a207d
CipherSoftRepo <38687589+CipherSoftRepo@users.noreply.github.com>
2021-08-13 11:24:38 +0200

    Product setup | tests for products

--- CodingTaskBRE.Tests/UnitTest1.cs
+++ CodingTaskBRE.Tests/UnitTest1.cs
@@ -9,10 +9,34 @@ namespace CodingTaskBRE.Tests
         {
         }
 
+     
         [Test]
-        public void Test1()
+        public void ShouldReturnBookSlipOnly()
         {
-            Assert.Pass();
+            var result = CodingTaskBRE.OrderSystem.GetProduct(new string[] { "Book", "Random" });
+            Assert.AreEqual("Random", result.Name);
+            Assert.AreEqual("Generated a packing slip for shipping.", result.Operations[0]);
+            Assert.AreEqual("Commission payment to the agent", result.Operations[1]);
+            Assert.AreEqual("Create a duplicate packing slip for the royalty department.", result.Operations[2]);
+            Assert.AreEqual(3, result.Operations.Count);
+
         }
+
+        [Test]
+        public void ShouldReturnOther()
+        {
+            var result = CodingTaskBRE.OrderSystem.GetProduct(new string[] { "other", "Random" });
+            Assert.AreEqual("Random", result.Name);
+            Assert.AreEqual("Generated a packing slip for shipping.", result.Operations[0]);
+            Assert.AreEqual("Commission payment to the agent", result.Operations[1]);
+            Assert.AreEqual(2, result.Operations.Count);
+
+            result = OrderSystem.GetProduct(new string[] { "random", "Random" });
+            Assert.AreEqual("Random", result.Name);
+            Assert.AreEqual("Generated a packing slip for shipping.", result.Operations[0]);
+            Assert.AreEqual("Commission payment to the agent", result.Operations[1]);
+            Assert.AreEqual(2, result.Operations.Count);
+        }
+
     }
 }
--- CodingTaskBRE/Program.cs
+++ CodingTaskBRE/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CodingTaskBRE
 {
+
+    public abstract class Product
+    {
+        public string Name { get; set; }
+        public abstract void GetSlip();
+        public List<string> Operations { get; set; } = new List<string>();
+    }
+    public abstract class PhysicalProduct : Product
+    {
+        public override void GetSlip()
+        {
+            Operations.Add("Generated a packing slip for shipping.");
+            Console.WriteLine("Generated a packing slip for shipping.");
+        }
+        public virtual void AddCommission()
+        {
+            Operations.Add("Commission payment to the agent");
+            Console.WriteLine("Commission payment to the agent");
+        }
+    }
+
+    public class Book : PhysicalProduct
+    {
+        public Book(string name)
+        {
+            Name = name;
+            base.GetSlip();
+            base.AddCommission();
+            GetSlip();
+        }
+        public override void GetSlip()
+        {
+            Operations.Add("Create a duplicate packing slip for the royalty department.");
+            Console.WriteLine("Create a duplicate packing slip for the royalty department.");
+        }
+    }
+
+    public class Other : PhysicalProduct
+    {
+        public Other(string name)
+        {
+            Name = name;
+            base.GetSlip();
+            base.AddCommission();
+        }
+    }
+
+    public class OrderSystem
+    {
+        public static Product GetProduct(string[] inputs)
+        {
+            return null;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Book book = new Book("ipsum");
         }
     }
 }
